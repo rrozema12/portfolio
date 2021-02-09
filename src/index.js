7e892b9e35040438e5589b809524783ef004c81b
Ryan Rozema <rrozema2@gmail.com>
2021-02-09 15:19:20 -0800

    use hash

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React, { Fragment, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 import MainBody from "./components/MainBody";
 import AboutMe from "./components/AboutMe";
 import Project from "./components/Project";
-import Navbar from "./components/Navbar";
 
 const Home = () => {
   useEffect(() => {
