d4d11f32c92769cabfdd998dbe6de2dbb21b985c
Ryan Rozema <rrozema2@gmail.com>
2021-07-12 16:43:07 -0700

    Updates

---
+++ precache-manifest.339617b5b8764c52a294e085030e7b06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d63c5a22acd2ae52123c51c44ae841",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b0cd110863009a814e30",
+    "url": "/portfolio/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "a7ff8c944bbd1627a229",
+    "url": "/portfolio/static/css/main.8c160b74.chunk.css"
+  },
+  {
+    "revision": "b0cd110863009a814e30",
+    "url": "/portfolio/static/js/2.0c2d86b5.chunk.js"
+  },
+  {
+    "revision": "44b6ea107d5f6954b31204b3600b6c0e",
+    "url": "/portfolio/static/js/2.0c2d86b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ff8c944bbd1627a229",
+    "url": "/portfolio/static/js/main.47a66fa6.chunk.js"
+  },
+  {
+    "revision": "208220a398825f118b38",
+    "url": "/portfolio/static/js/runtime-main.e7ff6ad2.js"
+  },
+  {
+    "revision": "72a44ff5e34755311e1e8e5210fd16df",
+    "url": "/portfolio/static/media/resume.72a44ff5.pdf"
+  }
+]);
